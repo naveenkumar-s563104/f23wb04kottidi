427f7b13a13369c25d219a04e7e0e51e5c098ae8
naveenkumar-s563104 <143102810+naveenkumar-s563104@users.noreply.github.com>
2023-10-31 21:18:40 -0500

    update

--- routes/dress.js
+++ routes/dress.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('dress', { title: 'Dress' });
+  res.render('dress', { title: 'Search Result Dress' });
 });
 
 module.exports = router;
